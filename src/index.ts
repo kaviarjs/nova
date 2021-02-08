74adb761acc6acaf2b233c326b7ba08b2ec78057
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-02-08 10:01:42 +0200

    Added export of astToBody function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./core/quickLinkers";
 export * from "./core/defs";
 export { default as intersectBody } from "./core/graphql/intersectDeep";
 export { mergeDeep as mergeBody } from "./core/graphql/mergeDeep";
+export { astToBody } from "./core/graphql/astToQuery";
