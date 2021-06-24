9e3d8040a88ff7c69939fb19c44ffe3b6abcbc17
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-06-24 08:52:26 +0300

    Exported constants

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from "./core/defs";
 export { default as intersectBody } from "./core/graphql/intersectDeep";
 export { mergeDeep as mergeBody } from "./core/graphql/mergeDeep";
 export { astToBody } from "./core/graphql/astToQuery";
+export * from "./core/constants";
